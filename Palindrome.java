380b9a6d96a37ced8b051e7002fc5518c91e02e9
NIVEDHA <nivedhaj21@gmail.com>
2026-07-29 07:27:53 +0530

    Refactor Palindrome.java to focus on palindrome check
    Removed commented-out prime checking code and retained palindrome checking logic.

--- Palindrome.java
+++ Palindrome.java
@@ -15,12 +15,6 @@ public class Palindrome {
     }
 }
 /*
-import java.util.Scanner;
-
-class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
         int n = sc.nextInt();
         int count = 0;
 
